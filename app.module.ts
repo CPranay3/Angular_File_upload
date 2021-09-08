4396c8010f892ec09ae8c3cffc9dcdb0c7217013
CPranay3 <89389346+CPranay3@users.noreply.github.com>
2021-09-08 17:36:40 +0530

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -1,21 +1,24 @@
+import { BrowserModule } from'@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+
+import { FileUploadComponent } from	'./file-upload/file-upload.component';
+
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FileUploadService } from './file-upload.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+	AppComponent,
+	FileUploadComponent,
+
+],
+imports: [
+	BrowserModule,
+	HttpClientModule,
+	
+],
+providers: [FileUploadService],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
